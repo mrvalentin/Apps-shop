663b3a1197762bb16aa7293218ca447ed03d597c
Robert Rytovuori <robert.rytovuori@gmail.com>
2014-12-05 17:57:47 +0000

    Update UserContainer.java

--- AppStore/src/appstore/UserContainer.java
+++ AppStore/src/appstore/UserContainer.java
@@ -1,29 +1,50 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ALL TRESPASSERS WILL BE PROSEC uuu Ted
  */
 
 package appstore;
 
-import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
- * @author Robert
+ * @mage Robert
  */
 public class UserContainer extends Container {
 
     public UserContainer() {
+        //uses Container constructor
         super("User");
     }
     
+    //calls method from Storage to store users ArrayList in this object
     public void storeUsers(){
         super.storeArrayList();
     }
     
+    //adds user to ArrayList and stores new arraylist
     public void addUser(User user){
         super.addObject(user);
         storeUsers();
     }
+    
+    //searches userlist for 
+    public User findUser(String username){
+        
+        //initializes iterator
+        Iterator it = l.iterator();
+        
+        //checks username while a user exists
+        while (it.hasNext()){
+            User u = (User) it.next();
+            if (u.getUsername().equals(username)){
+                return u;
+            }
+        }
+        
+        //returns null if no match is found
+        return null;
+    }
+    
+    
 }
