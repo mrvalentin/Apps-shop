49ab887ea19789eca36fc52fa2084146b27e86af
Robert Rytovuori <robert.rytovuori@gmail.com>
2014-12-05 17:55:23 +0000

    Update AppContainer.java

--- AppStore/src/appstore/AppContainer.java
+++ AppStore/src/appstore/AppContainer.java
@@ -1,14 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *ALL TRESPASSERS WILL BE PRO SEC uuu Ted
  */
 
 package appstore;
 
+import java.util.Iterator;
+
 /**
  *
- * @author Robert
+ * @mage Robert
  */
 public class AppContainer extends Container {
 
@@ -23,4 +23,23 @@ public class AppContainer extends Container {
     public void addApp(App app){
         super.addObject(app);
     }
+    
+    //searches userlist for 
+    public User findApp(String appname){
+        
+        //initializes iterator
+        Iterator it = l.iterator();
+        
+        //checks username while a user exists
+        while (it.hasNext()){
+            User u = (User) it.next();
+            if (u.getUsername().equals(username)){
+                return u;
+            }
+        }
+        
+        //returns null if no match is found
+        return null;
+    }
+
 }
