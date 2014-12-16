7d3a93b45f025d243f7e0b598b0e7892a650dd17
S. Stanev <svetlozar@xaoc.io>
2014-12-16 03:20:47 +0000

    Update Main.java

--- AppStore/src/appstore/Main.java
+++ AppStore/src/appstore/Main.java
@@ -1,79 +1,129 @@
+/**
+ * @project AppStore
+ * @name Main.java
+ * @package appstore
+ * @created 12-Dec-2014
+ * @author ioGhost
+ */
 package appstore;
 
-import appstore.containers.UserContainer;
 import java.util.Scanner;
+import appstore.containers.UserContainer;
 
-class Main {
-    
-    // main method
+public class Main {
+
     public static void main (String[] args) {
         
-        //creates bob user so that you can try logging in as bob
-        /*UserContainer users = new UserContainer();
-        User bob=new User("bob", "Boob", "lalavale", "roach");
-        users.add(bob);
-        users.store(); */
+        User user; // storing object of the current user (session)
+        Scanner input = new Scanner(System.in); // scanner object
         
-        //an object that represents the current user that is using the program
-        User user;
-        // keeping the application running
-        boolean run = true;
+        String command = ""; // store user input
         
-        // store the user input
-        String command;
+        boolean logged = false; // will run the login loop
+        boolean run = true; // will run the main loop
         
-        // initialize the scanner
-        Scanner userInput = new Scanner(System.in);
+        // welcome message
+        System.out.println("Welcome to AppStore version 0.1\n"
+                + "Created by Khalil, Valentin, Svetlozar & Robert");
         
-        // main loop
-        while (run == true) {
+        
+        
+        // login / registration loop
+        while (logged == false) {
             
-            // Display the main menu
-            System.out.println("1.0) User management");
-            System.out.println("\t1.1) Existing user, log-in");
-            System.out.println("\t1.2) New user, sign-up");
+            // fetch user input
+            System.out.println("Please type \"login\" "
+                    + "if you already have an account or"
+                    + " \"register\" to create new one.");
+            command = input.nextLine();
             
-            // Ask for user input
-            System.out.print("\nCommand: ");
-            command = userInput.nextLine();
-            
-            // start switch statements
-            switch (command) {
+            // login command
+            if (command.equals("login")) {
+                // let the manage user class deal with the login
+                user = ManageUser.login();
                 
-                // User login
-                case "1.1":
-                    //user login
-                    user = ManageUser.Login();
-                    
-                    
-                    //Login will be called twice with code below  Sincerely, Rob :P
-                    
-                    //If the user can't type his username correctly
-                    if(user == null)
-                        break;
-                    //If the user typed it correctly, the user object gets the info of the person who logged in
-                    user=ManageUser.Login();
-                    
-                    
-                // User registration
-                case "1.2":
-                    ManageUser.Registration();
-                    break;
-                    
-                case "exit":
-                    run = false;
-                    break;
-                    
-                default:
-                    System.out.println("Invalid command.\n");
-                    break;
+                // go out of the loop
+                logged = true;
+            }
+            
+            // registration command
+            else if (command.equals("register")) {
+                // let the manage user class deal with the registration
+                ManageUser.register();
+                
+                // go out of the loop
+                logged = true;
+            }
+            
+            // invalid command
+            else {
+                System.out.println("Invalid command, please try again.");
             }
             
         }
         
-        // Goodbye message
-        System.out.println("Thank you for using AppStore v0.1 \n"
-                + "Goodbye.");
-    } 
-    
+        
+        
+        // main loop
+        while (run == true) {
+            
+            // display main menu
+            System.out.println("The following commands are available:"
+                    + "APPS\n"
+                    + "\t1) add app\n"
+                    + "\t2) view app\n"
+                    + "\t3) update app\n"
+                    + "\t4 delete app\n"
+                    + "\n\n"
+                    + "USERS\n"
+                    + "\t1) view user\n"
+                    + "\t2) update user\n"
+                    + "\t3) delete user\n");
+            
+            // get user input
+            System.out.print("Command: ");
+            command = input.nextLine();
+            
+            
+            
+            // adding new app
+            if (command.equals("add app")) {
+                System.out.println("Adding new app...");
+            }
+            
+            // view app info
+            else if (command.equals("view app")) {
+                
+            }
+            
+            else if (command.equals("update app")) {
+
+            }
+
+            else if (command.equals("delete app")) {
+
+            }
+
+            else if (command.equals("view user")) {
+                //User.showProfile();
+            }
+
+            else if (command.equals("update user")) {
+
+            }
+
+            else if (command.equals("delete user")) {
+
+            }
+
+            else {
+                System.out.println("Invalid command.\n");
+            }
+            
+        }
+        
+        // goodbye message
+        System.out.println("Goodbye.");
+    }
+     
 }
