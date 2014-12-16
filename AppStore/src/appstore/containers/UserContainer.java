9579fccb8c7952756c3b52cf34f4d5044055411f
S. Stanev <svetlozar@xaoc.io>
2014-12-16 03:23:09 +0000

    Update UserContainer.java

--- AppStore/src/appstore/containers/UserContainer.java
+++ AppStore/src/appstore/containers/UserContainer.java
@@ -1,60 +1,71 @@
-/*
- * ALL TRESPASSERS WILL BE PROS
+/**
+ * @project AppStore
+ * @name UserContainer.java
+ * @package appstore.containers
+ * @created 12-Dec-2014
+ * @author ioGhost
  */
-
 package appstore.containers;
 
-import appstore.User;
 import java.util.Iterator;
+import appstore.User;
 
-/**
- *
- * @mage Robert
- */
 public class UserContainer extends Container {
 
+    // class constructor
     public UserContainer() {
-        //uses Container constructor
+        
+        // uses Container's constructor and passes
+        // 'user' as a name of the file the user information
+        // will be stored in.
         super("User");
     }
     
-    //calls method from Storage to store users ArrayList in this object
-    public void store(){
+    
+    
+    // sends ArrayList of instance to class Storage to be serialized into file
+    // ALL objects INSIDE the ArrayList object MUST be SERIALIZABLE
+    // ALL objects inside ArrayList should be of same class because they will be stored in a file 
+    // with the name of the class
+    public void store () {
         super.storeArrayList();
     }
     
-    //adds user to ArrayList and stores new arraylist
-    public void add(User user){
-        //checks if username already exists
-        if (this.find(user.getUsername())==null){
+    
+    
+    // adds user to ArrayList and stores new arraylist
+    public void add (User user) {
+        
+        // checks if username already exists
+        if (this.find(user.getUsername())== null) {
             super.add(user);
         }
     }
     
-    //searches userlist for username
-    public User find(String username){
+    // searches userlist for username
+    public User find (String username) {
         
-        //initializes iterator
+        // initializes iterator
         Iterator it = l.iterator();
         
-        //checks username while a user exists
-        while (it.hasNext()){
+        // checks username while a user exists
+        while (it.hasNext()) {
             User u = (User) it.next();
             if (u.getUsername().equals(username)){
                 return u;
             }
         }
         
-        //returns null if no match is found
+        // returns null if no match is found
         return null;
     }
     
-    //deletes user with given username
-    public void delete(String username){
+    
+    
+    // deletes user with given username
+    public void delete(String username) {
         User u = this.find(username);
         l.remove(u);
     }
-    
-    
     
 }
