55e56713c04046fea328f4d2730fd707c0b820b0
S. Stanev <svetlozar@xaoc.io>
2014-12-16 03:20:05 +0000

    Update User.java

--- AppStore/src/appstore/User.java
+++ AppStore/src/appstore/User.java
@@ -1,69 +1,83 @@
-//Valentin
-
+/**
+ * @project AppStore
+ * @name User.java
+ * @package appstore
+ * @created 12-Dec-2014
+ * @author ioGhost
+ */
 package appstore;
 
 public class User implements java.io.Serializable {
-  
-  private String username,name,address,profession;
-  private int trialUsed;
-  
-  //Constructors
-  public User(String username, String name, String address, String profession){
-      this.username = username;
-      this.name = name;
-      this.address = address;
-      this.profession = profession;
-      this.trialUsed = 0;
-  }
- 
-  public User(){
-      this.username ="";
-      this.name = "";
-      this.address = "";
-      this.profession = "";
-      this.trialUsed = 0;
-  }
-  // returns username
-  public String getUsername(){
-  return this.username;
-  }
-  //returns user'name
-  public String getName(){
-  return this.name;
-  }
-  //returns user' address
-  public String getAddress(){
-  return this.address;
-  }
-  // returns user' profession
-  public String getProfession(){
-  return this.profession;
-  }
-  //returns number of tiral used by the user
-  public int getTrialUsed(){
-  return this.trialUsed;
-  }
-  //setters
-  public void setUsername(String username){
-  this.username=username;
-  }
-  public void setName(String name){
-  this.name=name;
-  }
-  public void setAddress(String address){
-  this.address=address;
-  }
-  public void setProfession(String profession){
-  this.profession=profession;
-  }
-  public void setTrialUsed(int trialUsed){
-  this.trialUsed=trialUsed;
-  }
-  // Shows user information on user's profile
-  public void showProfile(){
-  System.out.println("Username: "+this.username+":\n"
-  +"Name:"+this.name+"\n"
-  +"Address:"+this.address
-  +"\n"+"Profession:"+this.profession+"\n");
-  }
+
+    private String username,name,address,profession;
+    private int trialUsed;
+
+    // constructor
+    public User(String username, String name, String address, String profession){
+        this.username = username;
+        this.name = name;
+        this.address = address;
+        this.profession = profession;
+        this.trialUsed = 0;
+    }
+
+    // default constructor
+    public User (){
+        this.username ="";
+        this.name = "";
+        this.address = "";
+        this.profession = "";
+        this.trialUsed = 0;
+    }
+
+    /*** start getters ***/
+    public String getUsername (){
+        return this.username;
+    }
+
+    public String getName (){
+        return this.name;
+    }
+
+    public String getAddress (){
+        return this.address;
+    }
+
+    public String getProfession (){
+        return this.profession;
+    }
+    
+    public int getTrialUsed (){
+        return this.trialUsed;
+    }
+    
+    /*** start setters ***/
+    public void setUsername (String username){
+        this.username=username;
+    }
+    
+    
+    public void setName (String name){
+        this.name=name;
+    }
+    
+    public void setAddress (String address){
+        this.address=address;
+    }
+    
+    public void setProfession (String profession){
+        this.profession=profession;
+    }
+    
+    public void setTrialUsed (int trialUsed){
+        this.trialUsed=trialUsed;
+    }
+    
+    // Shows user information on user's profile
+    public void showProfile () {
+        System.out.println("Username: "+this.username+":\n"
+        +"Name:"+this.name+"\n"
+        +"Address:"+this.address
+        +"\n"+"Profession:"+this.profession+"\n");
+    }
 }
