1f55187f4d295122d99444d695081528134438c5
S. Stanev <svetlozar@xaoc.io>
2014-12-16 03:22:25 +0000

    Update Storage.java

--- AppStore/src/appstore/io/Storage.java
+++ AppStore/src/appstore/io/Storage.java
@@ -1,74 +1,73 @@
-/*
- * ALL TRESPASSERS WILL BE PROS
+/**
+ * @project AppStore
+ * @name Storage.java
+ * @package appstore.io
+ * @created 12-Dec-2014
+ * @author ioGhost
  */
-
 package appstore.io;
 
 import java.io.*;
 
-/**
- *
- * @mage Robert
- */
 public class Storage {
     
-    //returns serializable object that has been stored in file
-    public static < E > E getObject(String filename){
+    // returns serializable object that has been stored in file
+    public static < E > E getObject (String filename){
         
-        //initializes object that will be returned
-        //E can be any object
+        // initializes object that will be returned
+        // E can be any object
         E object = null;
         
-        //error handling
+        //  error handling
         try {
-            //connects to file
+            //  onnects to file
             FileInputStream fileIn = new FileInputStream(filename+".ser");
             ObjectInputStream in = new ObjectInputStream(fileIn);
             
-            //returns boject in file
+            // returns boject in file
             object = (E) in.readObject();
             
-            //close connection to file
+            // close connection to file
             in.close();
             fileIn.close();
             
-        }catch(IOException | ClassNotFoundException e){
-            //prints error cause
-            //there may be better way to do this
+        }catch(IOException | ClassNotFoundException e) {
+            // prints error cause
+            // there may be better way to do this
             e.printStackTrace();
         }
         return object;
     }
     
-    //stores serializable object
-    //previous object with same filename will be OVERWRITTEN
-    public static < E > void storeObject(E object, String filename){
+    // stores serializable object
+    // previous object with same filename will be OVERWRITTEN
+    public static < E > void storeObject(E object, String filename) {
         try {
-            //connects to file
+            // connects to file
             FileOutputStream fileOut = new FileOutputStream(filename+".ser");
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             
-            //serializes and stores object into file
+            // serializes and stores object into file
             out.writeObject(object);
             
-            //closes connection to file
+            // closes connection to file
             out.close();
             fileOut.close();
         }
-        catch(IOException e){
-            //prints error cause
-            //there may be better way to do this
+        catch(IOException e) {
+            // prints error cause
+            // there may be better way to do this
             e.printStackTrace();
         }
     } 
     
-    //returns ture if file exists
-    public static boolean fileExists(String filename){
+    // returns ture if file exists
+    public static boolean fileExists(String filename) {
         
-        //path to file
+        // path to file
         File x = new File(filename+".ser");
         
-        //checks if file exists
+        // checks if file exists
         return x.exists();
     }
 }
