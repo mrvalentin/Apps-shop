ede67d928c8acd7efe65267d06d759d6319dd65d
S. Stanev <svetlozar@xaoc.io>
2014-12-18 22:33:41 +0000

    Update Main.java

--- AppStore/src/appstore/Main.java
+++ AppStore/src/appstore/Main.java
@@ -1,113 +1,71 @@
 /**
  * @project AppStore
- * @name Main.java
+ * @name AppStore.java
  * @package appstore
- * @created 12-Dec-2014
+ * @created 18-Dec-2014
  * @author ioGhost
  */
 package appstore;
 
 import java.util.Scanner;
 import appstore.containers.UserContainer;
-//import appstore.containers.AppContainer;
+import appstore.containers.AppContainer;
+import appstore.ManageUser;
+import appstore.ManageApp;
 
-public class Main {
+public class AppStore {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+    
         
-        User user; // storing object of the current user (session)
         Scanner input = new Scanner(System.in); // scanner object
         
-        String command = ""; // store user input
+        String command = ""; // storing user input
         
-        boolean logged = false; // will run the login loop
-        boolean run = true; // will run the main loop
+        boolean logged = false; // is the user logged or not
+        boolean run = true; // used for the main loop
         
         // welcome message
-        System.out.println("Welcome to AppStore version 0.1\n"
-                + "Created by Khalil, Valentin, Svetlozar & Robert");
-        
-        
-        
-        // login / registration loop
-        while (logged == false) {
+        System.out.println("Welcome to AppStore v0.1\n"
+                + "Created by Khalil, Valentin, Svetlozar & Robert");        
+
+         // login / register loop
+        while (run == true && logged == false) {
+            
+            // available commands
+            System.out.println("Available commands:\n"
+                    + "1) login\n"
+                    + "2) register\n"
+                    + "3) exit\n");
             
             // fetch user input
-            System.out.println("\n\nPlease type \"login\" "
-                    + "if you already have an account or"
-                    + " \"register\" to create new one.");
-            System.out.print("Command: ");
-            command = input.nextLine();
-            
-            // login command
-            if (command.equals("login")) {
-                // let the manage user class deal with the login
-                user = ManageUser.login();
-                
-                // go out of the loop, only if the user successfully logged
-                if (user != null) {
-                    logged = true;
-                }
-            }
-            
-            // registration command
-            else if (command.equals("register")) {
-                // let the manage user class deal with the registration
-                ManageUser.register();
-            }
-            
-            // invalid command
-            else {
-                System.out.println("Invalid command, please try again.");
-            }
-            
-        }
-        
-        
-        
-        // main loop
-        while (run == true) {
-            
-            // display main menu
-            System.out.println("The following commands are available:"
-                    + "APPS\n"
-                    + "\t1) add app\n"
-                    + "\t2) view app\n"
-                    + "\t3) update app\n"
-                    + "\t4 delete app\n"
-                    + "\n"
-                    + "USERS\n"
-                    + "\t1) view user\n"
-                    + "\t2) update user\n"
-                    + "\t3) delete user\n");
-            
-            // get user input
             System.out.print("Command: ");
             command = input.nextLine();
             
             
             
-            // switch between the commands
+            // switch statement
             switch (command) {
                 
-                // view user
-                case "view user":
+                case "login":
+                    ManageUser.login();
+                    break;
                     
-                    // ask for username
-                    System.out.print("Search for: ");
-                    command = input.nextLine();
+                case "register":
+                    ManageUser.register(); // proceed with user registration
+                    System.out.println("\nRegistration completed successfully."); // registration completed
+                    break;
                     
-                    // display profile information
-                    //UserContainer.userInformation(command);
+                case "exit":
+                    run = false;
+                    break;
                     
-                    System.out.println("\nPress enter to go back.");
-                
+                default:
+                    System.out.println("\nInvalid command.\n");
+                    break;
             }
             
-        }
-        
-        // goodbye message
-        System.out.println("Goodbye.");
+        }       
     }
-     
+    
 }
