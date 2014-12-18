8780dc1cb42b444214572b87ee8d2ec4b20278fb
Robert Rytovuori <robert.rytovuori@yahoo.com>
2014-12-18 20:43:52 +0000

    ShoppingCart0.2

--- AppStore/src/appstore/containers/ShoppingCart.java
+++ AppStore/src/appstore/containers/ShoppingCart.java
@@ -9,6 +9,7 @@ package appstore.containers;
 import appstore.App;
 import appstore.User;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
@@ -25,10 +26,36 @@ public class ShoppingCart {
         this.customer = U;
     }
     
-    //add app to shopping cart
-    public void addApp(App app){
-        l.add(app);
+    //searches shoppingcart for app with given name
+    public App find(String name){
+        
+        //initializes iterator
+        Iterator it = l.iterator();
+        
+        //checks app name while an app exists
+        while (it.hasNext()){
+            App a = (App) it.next();
+            if (a.getName().equals(name)){
+                return a;
+            }
+        }
+        
+        //returns null if no match is found
+        return null;
     }
     
+    //add app to shopping cart if its not already there
+    public void addApp(App app){
+        
+        String appname = app.getName();
+        
+        if (find(appname)==null){
+            l.add(app);
+        }
+    }
     
+    //deletes app with name from shopping list
+    public void deleteApp(App app){
+        l.remove(app);
+    }
 }
